71c2859d965271e3035d870cfb74988025141f0f
Iris Teyssier <iris.teyssier@gmail.com>
2024-05-03 12:35:35 -0700

    add files

---
+++ scripts/dynamic.js
@@ -0,0 +1,12 @@
+let colors = ["red", "orange", "tan", "rebeccapurple", "silver", "maroon", "purple", "#DEADBEEF", "F00BA4"]
+
+let ul = document.createElement("ul");
+for (let i = 0; i < colors.length; ++i) {
+  let li = document.createElement("li");
+  li.innerText = colors[i];
+  li.style.color = colors[i];
+  ul.appendChild(li);
+}
+
+let div = document.getElementById("dynamic");
+div.appendChild(ul);
